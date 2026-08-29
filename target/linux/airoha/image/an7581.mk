d697faa28fb6ac05cf1fba9b62955c1eeb991a52
WorkBuddy <wb@local>
2026-08-30 05:17:20 +0800

    airoha: nokia_xg-040g-md-3rdparty 修正为 UBI FIT(.itb) 镜像格式
    原定义设了 KERNEL_IN_UBI 却用 sysupgrade.bin/sysupgrade-tar(从非-UBI 的
    stock 设备抄来)，且缺 KERNEL := kernel-bin | gzip 与 KERNEL_INITRAMFS 两行，
    对 tcboot UBI 不会把内核写进 UBI kernel 卷。改为与官方 ubi 设备一致的
    sysupgrade.itb(FIT external-static-with-rootfs)，保证 sysupgrade -n 可刷。

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -190,18 +190,21 @@ endef
 TARGET_DEVICES += nokia_xg-040g-md-ubi
 
 define Device/nokia_xg-040g-md-3rdparty
-  $(call Device/nokia_xg-040g-md-common)
-  DEVICE_VARIANT := (3rdparty tcboot UBI)
-  DEVICE_DTS := an7581-nokia_xg-040g-md-3rdparty
-  UBOOTENV_IN_UBI := 1
-  KERNEL_IN_UBI := 1
-  IMAGES := sysupgrade.bin
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-  DEVICE_PACKAGES += kmod-airoha-xpon-en757x
-  SUPPORTED_DEVICES += nokia,xg-040g-md-ubi nokia,xg-040g-md
-  ARTIFACT/bl31-uboot.fip := an7581-bl31-uboot nokia_xg-040g-md
-  ARTIFACT/preloader.bin := an7581-preloader nokia_xg-040g-md
-  ARTIFACTS := bl31-uboot.fip preloader.bin
+	$(call Device/nokia_xg-040g-md-common)
+	DEVICE_VARIANT := (3rdparty tcboot UBI)
+	DEVICE_DTS := an7581-nokia_xg-040g-md-3rdparty
+	UBOOTENV_IN_UBI := 1
+	KERNEL_IN_UBI := 1
+	KERNEL := kernel-bin | gzip
+	KERNEL_INITRAMFS := kernel-bin | lzma | 	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 128k
+	KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+	IMAGES := sysupgrade.itb
+	IMAGE/sysupgrade.itb := append-kernel | 	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | 	append-metadata
+	DEVICE_PACKAGES += kmod-airoha-xpon-en757x
+	SUPPORTED_DEVICES += nokia,xg-040g-md-ubi nokia,xg-040g-md
+	ARTIFACT/bl31-uboot.fip := an7581-bl31-uboot nokia_xg-040g-md
+	ARTIFACT/preloader.bin := an7581-preloader nokia_xg-040g-md
+	ARTIFACTS := bl31-uboot.fip preloader.bin
 endef
 TARGET_DEVICES += nokia_xg-040g-md-3rdparty
 
